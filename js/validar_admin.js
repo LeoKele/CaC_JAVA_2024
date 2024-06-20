8190c8908720e00af906b808d1b224a75bc787e8
LeoKele <kelechian5@gmail.com>
2024-06-20 13:03:24 -0300

    Correcion campos Admin Peliculas

--- js/validar_admin.js
+++ js/validar_admin.js
@@ -4,36 +4,32 @@ document.getElementById("agregarPelicula").addEventListener("submit", function(e
     const titulo = document.getElementById("titulo");
     const genero = document.getElementById("genero");
     const duracion = document.getElementById("duracion").value;
-    const director = document.getElementById("director");
-    const reparto = document.getElementById("reparto");
-    const sinopsis = document.getElementById("sinopsis");
+    // const director = document.getElementById("director");
+    // const reparto = document.getElementById("reparto");
+    // const sinopsis = document.getElementById("sinopsis");
 
-    const fechaInput = document.getElementById("fecha");
-    const fecha = new Date(fechaInput.value); //creamos objeto del tipo Date para comparar la fecha ingresada con la fecha actual
-    const fechaActual = new Date();
-
-    // const pais = document.getElementById("country").value;
-
-    // const terminos = document.getElementById("terms");
+    // const fechaInput = document.getElementById("fecha");
+    // const fecha = new Date(fechaInput.value); //creamos objeto del tipo Date para comparar la fecha ingresada con la fecha actual
+    // const fechaActual = new Date();
 
     var errorTitulo = document.getElementById('mensajeTitulo');
-    var errorFecha = document.getElementById('mensajeFecha');
+    // var errorFecha = document.getElementById('mensajeFecha');
     var errorGenero = document.getElementById('mensajeGenero');
     var errorDuracion = document.getElementById('mensajeDuracion');
-    var errorDirector = document.getElementById('mensajeDirector');
-    var errorReparto = document.getElementById('mensajeReparto');
-    var errorSinopsis = document.getElementById('mensajeSinopsis');
+    // var errorDirector = document.getElementById('mensajeDirector');
+    // var errorReparto = document.getElementById('mensajeReparto');
+    // var errorSinopsis = document.getElementById('mensajeSinopsis');
     var errorPortada = document.getElementById('mensajePortada');
 
     // Limpio mensajes de error previos
     function limpiarMensajes(){
         errorTitulo.textContent = '';
-        errorFecha.textContent = '';
+        // errorFecha.textContent = '';
         errorGenero.textContent = '';
         errorDuracion.textContent = '';
-        errorDirector.textContent = '';
-        errorReparto.textContent = '';
-        errorSinopsis.textContent = '';
+        // errorDirector.textContent = '';
+        // errorReparto.textContent = '';
+        // errorSinopsis.textContent = '';
         errorPortada.textContent = '';
     }
     limpiarMensajes();
@@ -58,33 +54,39 @@ document.getElementById("agregarPelicula").addEventListener("submit", function(e
       }
       
     const tituloValido = validarInput(titulo);
-    const sinopsisValido = validarInput(sinopsis);
+    // const sinopsisValido = validarInput(sinopsis);
 
     const generoValido = validarInput(genero);
-    const directorValido = validarInput(director);
-    const repartoValido = validarInput(reparto);
+    // const directorValido = validarInput(director);
+    // const repartoValido = validarInput(reparto);
 
 
     //validar duracion
-    function validarEntero(texto){
-        var regex = /^[0-9]+$/;
-        return regex.test(texto);
+    function transformarTiempo(input) {
+        const tiempo = input.trim(); // Elimina espacios en blanco al principio y al final
+        const regex = /^(\d+)h?\s*(\d*)m?$/; // Expresión regular para analizar el formato
+    
+        const match = tiempo.match(regex);
+        if (!match) {
+            return false;
+        }
+    
+        const horas = parseInt(match[1]);
+        const minutos = parseInt(match[2] || 0);
+    
+        if (horas < 0 || minutos < 0 || minutos >= 60) {
+            return false;
+        }
+    
+        return `${horas}h ${minutos}m`;
     }
     
-    // function convertirAEntero(texto){
-    //     duracionValido = validarEntero(texto)
-    //     if(duracionValido){
-    //         return parseInt(texto, 10); 
-    //     } else {
-    //         return duracionValido;
-    //     }
-    // }
     
-    duracionValido = validarEntero(duracion);
+    duracionValido = transformarTiempo(duracion);
 
-    //valido date
-    fechaActual.setHours(0,0,0,0); // Establezco la hora de la fecha actual a 00:00:00 para comparar solo la fecha
-    const fechaValido = fecha < fechaActual ? true : false;
+    // //valido date
+    // fechaActual.setHours(0,0,0,0); // Establezco la hora de la fecha actual a 00:00:00 para comparar solo la fecha
+    // const fechaValido = fecha < fechaActual ? true : false;
 
 
     //* Valido que el input portada sea .jpg o .jpeg y que no esté vacio
@@ -113,7 +115,7 @@ document.getElementById("agregarPelicula").addEventListener("submit", function(e
 
 
     // Verifica si ambos campos son válidos
-    if (tituloValido && fechaValido && generoValido && duracionValido && directorValido && repartoValido && sinopsisValido && portadaValido) {
+    if (tituloValido && generoValido && duracionValido && portadaValido) {
         console.log("Formulario válido");
 
     } else {
@@ -121,12 +123,12 @@ document.getElementById("agregarPelicula").addEventListener("submit", function(e
 
         // Muestro los mensajes de error debajo de los campos
         document.getElementById("mensajeTitulo").textContent = tituloValido ? "" : "Por favor, completa este campo.";
-        document.getElementById("mensajeFecha").textContent = fechaValido ? "" : "Elija una fecha valida"
+        // document.getElementById("mensajeFecha").textContent = fechaValido ? "" : "Elija una fecha valida"
         document.getElementById("mensajeGenero").textContent = generoValido ? "" : "Por favor, completa este campo.";
-        document.getElementById("mensajeDuracion").textContent = duracionValido ? "" : "Ingrese un número valido.";
-        document.getElementById("mensajeDirector").textContent = directorValido ? "" : "Por favor, completa este campo.";
-        document.getElementById("mensajeReparto").textContent = repartoValido ? "" : "Por favor, completa este campo.";
-        document.getElementById("mensajeSinopsis").textContent = sinopsisValido ? "" : "Por favor, completa este campo.";
+        document.getElementById("mensajeDuracion").textContent = duracionValido ? "" : "Formato de tiempo no válido. Debe ser '90m' o '1h 30m'.";
+        // document.getElementById("mensajeDirector").textContent = directorValido ? "" : "Por favor, completa este campo.";
+        // document.getElementById("mensajeReparto").textContent = repartoValido ? "" : "Por favor, completa este campo.";
+        // document.getElementById("mensajeSinopsis").textContent = sinopsisValido ? "" : "Por favor, completa este campo.";
         document.getElementById("mensajePortada").textContent = portadaValido ? "" : "Por favor, ingrese una imagen";
     }
     
